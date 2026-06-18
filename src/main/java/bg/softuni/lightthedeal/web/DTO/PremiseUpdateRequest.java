f09b6278873bd9882ced7ac211dc420819a9bfc3
Rumyana Filipova <your-github-rumifilipova@gmail.com>
2026-06-18 21:16:18 +0300

    implemented modifications

--- src/main/java/bg/softuni/lightthedeal/web/DTO/PremiseUpdateRequest.java
+++ src/main/java/bg/softuni/lightthedeal/web/DTO/PremiseUpdateRequest.java
@@ -5,20 +5,23 @@ import bg.softuni.lightthedeal.premise.entity.PremiseType;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
+import java.util.UUID;
+
 @Builder
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 
-public class PremiseUpdateRequest{
+public class PremiseUpdateRequest {
 
 
-        @NotBlank(message = "* requered")
-        private String name;
-        private String address;
-        private String description;
-        private PremiseType type;
-        private Double area;
-        private Customer customer;
+    private UUID id;
+    @NotBlank(message = "* requered")
+    private String name;
+    private String address;
+    private String description;
+    private PremiseType type;
+    private Double area;
+    private Customer customer;
 }
